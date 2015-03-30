9292bc7ac9a0f437b76da38b0fe06ae9420d42e9
Jon Gillies <jgillies@macbookprodisplay.jonslab.com>
2015-03-29 19:17:02 -0700

    logic

--- transmission/updateTransmissionBindAddress.sh
+++ transmission/updateTransmissionBindAddress.sh
@@ -4,15 +4,22 @@ CURRENT_VPNADDR=`grep bind-address-ipv4 /etc/transmission-daemon/settings.json |
 
 VPNADDR=`ifconfig | grep -A 5 "tun" | grep "inet" | cut -f2 -d ':' | awk '{print $1}'`
 
-if [ -z "$VPNADDR" ]; then
+if [ -z "$CURRENT_VPNADDR" ]; then
     CURRENT_VPNADDR=127.0.0.1
 fi
 
+if [ -z "$VPNADDR" ]; then
+    VPNADDR=127.0.0.1
+fi
+
+if [ "$VPNADDR" == "0.0.0.0" ]; then
+    VPNADDR=127.0.0.1
+fi
+
 if [ "$CURRENT_VPNADDR" != "$VPNADDR" ]; then
-  echo $CURRENT_VPNADDR | cat -vet >> /logs/debug
-  echo $VPNADDR | cat -vet >> /logs/debug
   echo "`date` Restarting Tranmission... old addr $CURRENT_VPNADDR new addr $VPNADDR" >> /logs/transmission.log
+  /etc/init.d/transmission-daemon stop
   sed -i "s/.*bind-address-ipv4.*/    \"bind-address-ipv4\"\: \"$VPNADDR\",/g" /etc/transmission-daemon/settings.json
-  /etc/init.d/transmission-daemon restart
+  /etc/init.d/transmission-daemon start
 fi
 
